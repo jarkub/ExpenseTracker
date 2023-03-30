8d22e75f4d1627dc62ecb2424fba6fd930b6cd35
Jared Kubly <jared.kubly@gmail.com>
2023-03-29 19:12:53 -0600

    20230328

---
+++ expense-tracker-react/src/context/AppReducer.js
@@ -0,0 +1,15 @@
+// Reducer: specify app state changes in response to actions on context
+export default (state, action) => {
+  switch (action.type) {
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id != action.payload
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
